e1b032b794140068d82cddc36fe5840097b8d08a
CyberMaryVer <maria.s.startseva@gmail.com>
2024-04-20 15:49:16 +0300

    switch to TK

--- utils/api_requests.py
+++ utils/api_requests.py
@@ -29,7 +29,7 @@ def get_faiss_index(index_path=None):
     index_path = index_path if index_path else INDEX_PATH
     return FAISS.load_local(folder_path=index_path,
                             embeddings=OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY),
-                            # allow_dangerous_deserialization=True
+                            allow_dangerous_deserialization=True
                             )
 
 
